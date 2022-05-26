937b2c6fd8a23bf344ae07eb520a3c7e36439ab0
Meghana.S <97089631+Meghana-sketch141@users.noreply.github.com>
2022-05-26 16:02:08 +0000

    p11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,14 @@
-# Tuples
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+fh = open(name)
+counts = dict()
 
-filename = "dataset/mbox-short.txt"
+for line in fh:
+    if line.startswith('From ') :
+        words = line.split()
+        time = words[5]
+        hours = time[:2]
+        counts[hours] = counts.get(hours,0) + 1
+
+for key, val in sorted(counts.items()):
+    print (key, val)
