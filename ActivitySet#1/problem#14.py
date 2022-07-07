1566ab30a5217439429ca69053b6df2656c7f947
Meghana.S <97089631+Meghana-sketch141@users.noreply.github.com>
2022-07-07 16:35:40 +0000

    p17 16 15

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,12 @@
-# Using Web Services
-# https://www.py4e.com/lessons/servces
+num = int(input("Please Enter the Number you wish: "))
+
+if(num%4 == 0):
+    if(num%100 == 0):
+        if(num%400 == 0):
+            print("%d is a Leap Year" %num)
+        else:
+            print("%d is Not" %num)
+    else:
+        print("%d is a Leap Year" %num)
+else:
+    print("%d is Not" %num)
